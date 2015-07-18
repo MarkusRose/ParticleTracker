93edc2770d051ca4cf0a966d94011f9cdf0274b0
Markus Rose <markus.m.rose@gmail.com>
2015-07-17 21:27:49 -0400

    Creation of Setup.

--- enzymeDiffuser.py
+++ enzymeDiffuser.py
@@ -17,13 +17,21 @@ Particle1 = [frame, dx, dy, x, y, state, Intensity,
  Background, sigma_x, sigma_y, Particle ID]
 '''
 
+#Read in Values from file
 sV = Fileio.getSysProps()
-D = np.array([sV[0],sV[1],sV[2]])
-p = np.array([[0,sV[3],sV[5]],[sV[4],0,sV[6]],[sV[7],sV[8],0]])
+#Diff constants of 3 states
+D = np.array([sV[0],sV[1],sV[2]]) 
+#Markov probability matrix for state switching
+p = np.array([[0,sV[3],sV[5]],[sV[4],0,sV[6]],[sV[7],sV[8],0]]) 
+#Number of frames
 frames = int(sV[9])
+#Number of particles
 N = int(sV[10])
+#Acquisition time
 tau = sV[11]
+#Number of pixels (side of square)
 numPixels = int(sV[12])
+#Other properties
 wavelength = sV[13]
 pixel_size = sV[14]
 numAperture = sV[15]
@@ -31,22 +39,39 @@ mag = sV[16]
 sigToNoise = sV[17]
 intensity = sV[18]
 
+#Initial probabilities for choosing state
+'''
+pi1 = ( p21 + p31 ) / (p12 + p13 + p21 + p23 + p31 + p32)
+pi2 = ( p12 + p32 ) / (p12 + p13 + p21 + p23 + p31 + p32)
+pi3 = ( p13 + p23 ) / (p12 + p13 + p21 + p23 + p31 + p32)
+'''
 statProbs = []
 sumprobs = sV[3]+sV[4]+sV[5]+sV[6]+sV[7]+sV[8]
 statProbs.append((sV[4]+sV[7])/sumprobs)
 statProbs.append((sV[3]+sV[8])/sumprobs)
 statProbs.append((sV[5]+sV[6])/sumprobs)
 
+#output variable
 atracks = []
-
 for n in xrange(N):
+    #one individual track
     track = []
+
     for i in xrange(0,frames,1):
+
+        #particle in one frame
         particle = np.zeros((11))
+
+        #frame number
         particle[0] = i
+
+        #first or not?
         if len(track)  == 0:
+            #initial position
             particle[3] = 0
             particle[4] = 0
+
+            #choose state
             u = random.random()
             if u < statProbs[2]:
                 particle[5] = 2
@@ -54,9 +79,14 @@ for n in xrange(N):
                 particle[5] = 1
             else:
                 particle[5] = 0
+
         else:
+
+            #Calc new absolut position
             particle[3] = track[-1][3] + track[-1][1]
             particle[4] = track[-1][4] + track[-1][2]
+
+            #choose new state
             u = random.random()
             s0 = track[-1][5]
             s1 = (s0+1) % 3
@@ -67,13 +97,23 @@ for n in xrange(N):
                 particle[5] = s1
             else:
                 particle[5] = s0
+
+        #Generate displacement in correct state
         particle[1] = random.gauss(0,2*D[particle[5]]*tau)
         particle[2] = random.gauss(0,2*D[particle[5]]*tau)
+
+        #Rest of particle variables
         for k in xrange(6,10,1):
             particle[k] = 0
+
         particle[10] = n
+
+        #Append particle to track
         track.append(particle)
+
+    #Append Track to output var
     atracks.append(track)
 print atracks
-    
+
+#Print tracks to file
 Fileio.setTrackFile(atracks)
