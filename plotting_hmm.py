aed2956d9b52e5f3e65f094289912706e962d212
Markus Rose <markus.m.rose@gmail.com>
2017-02-17 11:00:39 -0500

    Added a Todo file and did some changes to Plotting_hmm

--- plotting_hmm.py
+++ plotting_hmm.py
@@ -13,12 +13,70 @@ import random
 
 
 path = "/media/markus/DataPartition/Cellulases-Analysis/"
-SR = 5
+SR = 1.5
 Cel = "5A"
 trackfile = "foundTracks-Cel{0:}-SR{1:}.txt".format(Cel,SR)
 hmmfile = "hmmAveragedData-Cel{0:}-SR{1:}.txt".format(Cel,SR)
 
-minlength = 100
+small = 20
+large = 100
+
+pixel_size = 0.067 #um
+timestep = 1 #s
+Dfactor = pixel_size**2/timestep
+
+if Cel == "5A":
+    if SR == 5:
+        boxup = np.array([0.06,0.094,1.01,1.7]) * Dfactor
+        boxdown = np.array([0.05,0.09,0.22,0.4]) * Dfactor
+        boxmid = np.array([0.12,0.13,0.1,0.3]) * Dfactor
+    elif SR == 3:
+        boxup = np.array([0.06,0.094,1.01,1.7]) * Dfactor
+        boxdown = np.array([0.05,0.09,0.22,0.4]) * Dfactor
+        boxmid = np.array([0.12,0.13,0.1,0.3]) * Dfactor
+    elif SR == 2:
+        boxup = np.array([0.00020,0.00035,0.0015,0.003])
+        boxdown = np.array([0.00014,0.00025,0.0007,0.0012])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+    elif SR == 1.5:
+        boxup = np.array([0.00025,0.0004,0.0010,0.0022])
+        boxdown = np.array([0.00014,0.00025,0.0005,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+elif Cel == "6B":
+    if SR == 5:
+        boxup = np.array([0.00010,0.00027,0.002,0.008])
+        boxdown = np.array([0.00005,0.00018,0.0003,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+    elif SR == 3:
+        boxup = np.array([0.00010,0.00025,0.0015,0.004])
+        boxdown = np.array([0.00005,0.00018,0.0003,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+    elif SR == 2:
+        boxup = np.array([0.00010,0.00025,0.0015,0.004])
+        boxdown = np.array([0.00005,0.00018,0.0003,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+    elif SR == 1.5:
+        boxup = np.array([0.00013,0.0003,0.001,0.0025])
+        boxdown = np.array([0.00005,0.00013,0.0005,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+elif Cel == "9A":
+    if SR == 5:
+        boxup = np.array([0.00007,0.0004,0.002,0.015])
+        boxdown = np.array([0.00005,0.00018,0.0003,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+    elif SR == 3:
+        boxup = np.array([0.00007,0.0004,0.001,0.01])
+        boxdown = np.array([0.00005,0.00018,0.0003,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+    elif SR == 2:
+        boxup = np.array([0.00007,0.0004,0.001,0.004])
+        boxdown = np.array([0.00005,0.00018,0.0003,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+    elif SR == 1.5:
+        boxup = np.array([0.00007,0.0004,0.001,0.0025])
+        boxdown = np.array([0.00005,0.00018,0.0003,0.001])
+        boxmid = np.array([0.12,0.13,0.1,0.3])
+
 
 
 class markovChain(object):
@@ -99,9 +157,15 @@ def readInFile(filename):
 
 if __name__=="__main__":
 
+    Dmin = 0.01**2/(4*timestep)
+    Dmax = (SR*pixel_size)**2/(4*timestep)
+
     if not os.path.isdir(path):
         os.mkdir(path)
     os.chdir(path)
+
+    Dfactor = pixel_size*pixel_size/timestep
+
 
     hmmdata = readInFile(hmmfile)
     tracks,z = ctrack.readTrajectoriesFromFile(trackfile,minTrackLen=1)
@@ -111,40 +175,160 @@ if __name__=="__main__":
 
     print len(hmmdata.hmm[hmmdata.hmm['length'] > 100]['D1'])
 
-    box = (0.12,0.13,0.1,0.3)
-    indeces = np.where(np.logical_and(hmmdata.hmm['length'] > 10,
-        np.logical_and(np.logical_and(hmmdata.hmm['D1'] > box[0], hmmdata.hmm['D1'] < box[1]),
-            np.logical_and(hmmdata.hmm['D2'] > box[2], hmmdata.hmm['D2'] < box[3]))))
+    indeces = []
+    
+    for box in [boxup,boxdown]:
+        indeces.append(np.where(np.logical_and(hmmdata.hmm['length'] > large,
+            np.logical_and(np.logical_and(hmmdata.hmm['D1'] > box[0]/Dfactor, hmmdata.hmm['D1'] < box[1]/Dfactor),
+                np.logical_and(hmmdata.hmm['D2'] > box[2]/Dfactor, hmmdata.hmm['D2'] < box[3]/Dfactor)))))
 
 
-    print hmmdata.hmm[indeces[0][0]]
-    print hmmdata.id[indeces[0][0]]
+    print hmmdata.hmm[indeces[0][0][0]]
+    print hmmdata.id[indeces[0][0][0]]
 
-    print tracks[indeces[0][0]].id
+    print tracks[indeces[0][0][0]].id
 
 
+    smallhmm = np.logical_and(hmmdata.hmm['length'] <= small,hmmdata.hmm['length'] > 1)
+    mediumhmm = np.logical_and(hmmdata.hmm['length'] <= large,hmmdata.hmm['length'] > small)
+    largehmm = hmmdata.hmm['length'] > large
+
+
+
+    savepng = os.path.join(path,"Tracks-Cel{:}-SR{:}".format(Cel,SR))
+    if not os.path.isdir(savepng):
+        os.mkdir(savepng)
+    os.chdir(savepng)
+
+    ofile = open("hmmStatistics-Cel{:}-SR{:}.txt".format(Cel,SR),'w')
+
+    ofile.write("Statistics for Cel{:} with a search Radius of {:}\n".format(Cel,SR))
+    ofile.write("=================================================\n\n")
+    ofile.write("Mean D1 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm['D1']*Dfactor),np.std(hmmdata.hmm['D1']*Dfactor)))
+    ofile.write("Mean D2 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm['D2']*Dfactor),np.std(hmmdata.hmm['D2']*Dfactor)))
+    ofile.write("Mean p12 = {:} +- {:} \n".format(np.mean(hmmdata.hmm['p12']),np.std(hmmdata.hmm['p12'])))
+    ofile.write("Mean p21 = {:} +- {:} \n\n".format(np.mean(hmmdata.hmm['p21']),np.std(hmmdata.hmm['p21'])))
+
+    ofile.write("Short tracks with length <= {:} steps\n".format(small))
+    ofile.write("-------------------------------------\n")
+    ofile.write("Mean D1 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm[smallhmm]['D1']*Dfactor),np.std(hmmdata.hmm[smallhmm]['D1']*Dfactor)))
+    ofile.write("Mean D2 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm[smallhmm]['D2']*Dfactor),np.std(hmmdata.hmm[smallhmm]['D2']*Dfactor)))
+    ofile.write("Mean p12 = {:} +- {:} \n".format(np.mean(hmmdata.hmm[smallhmm]['p12']),np.std(hmmdata.hmm[smallhmm]['p12'])))
+    ofile.write("Mean p21 = {:} +- {:} \n\n".format(np.mean(hmmdata.hmm[smallhmm]['p21']),np.std(hmmdata.hmm[smallhmm]['p21'])))
+
+    ofile.write("Medium tracks with {:} < length <= {:} steps\n".format(small,large))
+    ofile.write("--------------------------------------------\n")
+    ofile.write("Mean D1 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm[mediumhmm]['D1']*Dfactor),np.std(hmmdata.hmm[mediumhmm]['D1']*Dfactor)))
+    ofile.write("Mean D2 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm[mediumhmm]['D2']*Dfactor),np.std(hmmdata.hmm[mediumhmm]['D2']*Dfactor)))
+    ofile.write("Mean p12 = {:} +- {:} \n".format(np.mean(hmmdata.hmm[mediumhmm]['p12']),np.std(hmmdata.hmm[mediumhmm]['p12'])))
+    ofile.write("Mean p21 = {:} +- {:} \n\n".format(np.mean(hmmdata.hmm[mediumhmm]['p21']),np.std(hmmdata.hmm[mediumhmm]['p21'])))
+
+    ofile.write("Long tracks with length > {:} steps\n".format(large))
+    ofile.write("------------------------------------\n")
+    ofile.write("Mean D1 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm[largehmm]['D1']*Dfactor),np.std(hmmdata.hmm[largehmm]['D1']*Dfactor)))
+    ofile.write("Mean D2 = {:} +- {:} um^2 s^-1\n".format(np.mean(hmmdata.hmm[largehmm]['D2']*Dfactor),np.std(hmmdata.hmm[largehmm]['D2']*Dfactor)))
+    ofile.write("Mean p12 = {:} +- {:} \n".format(np.mean(hmmdata.hmm[largehmm]['p12']),np.std(hmmdata.hmm[largehmm]['p12'])))
+    ofile.write("Mean p21 = {:} +- {:} \n\n".format(np.mean(hmmdata.hmm[largehmm]['p21']),np.std(hmmdata.hmm[largehmm]['p21'])))
+
+    ofile.close()
+
+    averages = np.zeros((8,4))
+    averages[0,0] = np.mean(hmmdata.hmm['D1']*Dfactor)
+    averages[1,0] = np.mean(hmmdata.hmm['D2']*Dfactor)
+    averages[2,0] = np.mean(hmmdata.hmm['p12'])
+    averages[3,0] = np.mean(hmmdata.hmm['p21'])
+    averages[4,0] = np.std(hmmdata.hmm['D1']*Dfactor)
+    averages[5,0] = np.std(hmmdata.hmm['D2']*Dfactor)
+    averages[6,0] = np.std(hmmdata.hmm['p12'])
+    averages[7,0] = np.std(hmmdata.hmm['p21'])
+    averages[0,1] = np.mean(hmmdata.hmm[smallhmm]['D1']*Dfactor)
+    averages[1,1] = np.mean(hmmdata.hmm[smallhmm]['D2']*Dfactor)
+    averages[2,1] = np.mean(hmmdata.hmm[smallhmm]['p12'])
+    averages[3,1] = np.mean(hmmdata.hmm[smallhmm]['p21'])
+    averages[4,1] = np.std(hmmdata.hmm[smallhmm]['D1']*Dfactor)
+    averages[5,1] = np.std(hmmdata.hmm[smallhmm]['D2']*Dfactor)
+    averages[6,1] = np.std(hmmdata.hmm[smallhmm]['p12'])
+    averages[7,1] = np.std(hmmdata.hmm[smallhmm]['p21'])
+    averages[0,2] = np.mean(hmmdata.hmm[mediumhmm]['D1']*Dfactor)
+    averages[1,2] = np.mean(hmmdata.hmm[mediumhmm]['D2']*Dfactor)
+    averages[2,2] = np.mean(hmmdata.hmm[mediumhmm]['p12'])
+    averages[3,2] = np.mean(hmmdata.hmm[mediumhmm]['p21'])
+    averages[4,2] = np.std(hmmdata.hmm[mediumhmm]['D1']*Dfactor)
+    averages[5,2] = np.std(hmmdata.hmm[mediumhmm]['D2']*Dfactor)
+    averages[6,2] = np.std(hmmdata.hmm[mediumhmm]['p12'])
+    averages[7,2] = np.std(hmmdata.hmm[mediumhmm]['p21'])
+    averages[0,3] = np.mean(hmmdata.hmm[largehmm]['D1']*Dfactor)
+    averages[1,3] = np.mean(hmmdata.hmm[largehmm]['D2']*Dfactor)
+    averages[2,3] = np.mean(hmmdata.hmm[largehmm]['p12'])
+    averages[3,3] = np.mean(hmmdata.hmm[largehmm]['p21'])
+    averages[4,3] = np.std(hmmdata.hmm[largehmm]['D1']*Dfactor)  
+    averages[5,3] = np.std(hmmdata.hmm[largehmm]['D2']*Dfactor)
+    averages[6,3] = np.std(hmmdata.hmm[largehmm]['p12'])
+    averages[7,3] = np.std(hmmdata.hmm[largehmm]['p21'])
+
+    fig4,axs = plt.subplots(nrows=2,ncols=2, sharex=True)
+    ax = axs[0,0]
+    ax.errorbar(np.arange(4),averages[0],yerr=averages[0+4],fmt='o-',color='r')
+    ax.set_xlabel("Length Category")
+    ax.set_ylabel(r"D1 in $\mu m^2s^{-1}$")
+    ax2= axs[0,1]
+    ax2.errorbar(np.arange(4),averages[1],yerr=averages[1+4],fmt='o-',color='r')
+    ax2.set_xlabel("Length Category")
+    ax2.set_ylabel(r"D2 in $\mu m^2s^{-1}$")
+    ax3= axs[1,0]
+    ax3.errorbar(np.arange(4),averages[2],yerr=averages[2+4],fmt='o-',color='r')
+    ax3.set_xlabel("Length Category")
+    ax3.set_ylabel(r"p12")
+    ax4 = axs[1,1]
+    ax4.errorbar(np.arange(4),averages[3],yerr=averages[3+4],fmt='o-',color='r')
+    ax4.set_xlabel("Length Category")
+    ax4.set_ylabel(r"")
+    plt.savefig("lengthsCel{:}SR{:}.png".format(Cel,SR))
+    plt.show()
+
+
+    plt.hist(hmmdata.hmm[largehmm]['length'],50)
+    plt.show()
 
     fig2 = plt.figure()
     ax2 = fig2.add_subplot(111, aspect='equal')
-    ax2.loglog(hmmdata.hmm[hmmdata.hmm['length'] > 1]['D1'],hmmdata.hmm[hmmdata.hmm['length'] > 1]['D2'],'y+',zorder=1)
-    ax2.loglog(hmmdata.hmm[hmmdata.hmm['length'] > 10]['D1'],hmmdata.hmm[hmmdata.hmm['length'] > 10]['D2'],'b.',zorder=2)
-    ax2.loglog(hmmdata.hmm[hmmdata.hmm['length'] > 100]['D1'],hmmdata.hmm[hmmdata.hmm['length'] > 100]['D2'],'ro',zorder=3)
+    ax2.loglog(hmmdata.hmm[smallhmm]['D1']*Dfactor,hmmdata.hmm[smallhmm]['D2']*Dfactor,'y.',zorder=1,label=r"$\Delta t<${:}".format(small*timestep))
+    ax2.loglog(hmmdata.hmm[mediumhmm]['D1']*Dfactor,hmmdata.hmm[mediumhmm]['D2']*Dfactor,'b.',zorder=2,label=r"{:}$<\Delta t<${:}".format(small*timestep,large*timestep))
+    ax2.loglog(hmmdata.hmm[largehmm]['D1']*Dfactor,hmmdata.hmm[largehmm]['D2']*Dfactor,'r^',zorder=3,label=r"$\Delta t>${:}".format(large*timestep))
+    for box in [boxup,boxdown]:
+        ax2.add_patch(
+                patches.Rectangle(
+                    (box[0], box[2]),
+                    box[1]-box[0],
+                    box[3]-box[2],
+                    fill=False,      # remove background
+                    linewidth=2,
+                    zorder=10
+                    )
+                )
     ax2.add_patch(
             patches.Rectangle(
-                (box[0], box[2]),
-                box[1]-box[0],
-                box[3]-box[2],
+                (Dmin, Dmin),
+                Dmax-Dmin,
+                Dmax-Dmin,
                 fill=False,      # remove background
-                linewidth=3,
-                zorder=10
+                linewidth=2,
+                zorder=10,
+                color='0.75'
                 )
             )
-    plt.show()
+    ax2.legend()
+    plt.xlabel(r"D1 in $\mu m^2 s^{-1}$")
+    plt.ylabel(r"D2 in $\mu m^2 s^{-1}$")
+    plt.title("Cel{0:}-SR{1:}".format(Cel,SR))
+    plt.savefig("hmm-Cel{0:}-SR{1:}.png".format(Cel,SR))
+    plt.show(block=True)
     
 
+    '''
     fig = plt.figure()
-    for i in xrange(len(indeces[0])):
-        tra = np.array(tracks[indeces[0][i]].track[np.invert(np.isnan(tracks[indeces[0][i]].track['x']))])
+    for index in indeces[0][0]:
+        tra = np.array(tracks[index].track[np.invert(np.isnan(tracks[index].track['x']))])
         z = tra['frame']-tra[0]['frame']
         x = tra['x']
         y = tra['y']
@@ -157,4 +341,57 @@ if __name__=="__main__":
     plt.axis([0,400,0,400])
     axcb = fig.colorbar(lc)
     axcb.set_label('frame')
-    plt.show()
+    plt.show(block=True)
+    '''
+    
+
+    userinput = 'n'
+    userinput = raw_input("Create plots of Tracks? [y,N]  ") 
+    num = 0
+    plt.ioff()
+
+    if userinput == 'y':
+        num = 2
+
+        for i in xrange(len(indeces)):
+            spng = os.path.join(savepng,"Box-{:}".format(i))
+            if not os.path.isdir(spng):
+                os.mkdir(spng)
+            os.chdir(spng)
+            difference = 0
+            j = 0
+            while j < len(indeces[i][0]):
+                if hmmdata.id[indeces[i][0][j]] != tracks[indeces[i][0][j]+difference].id:
+                    difference += 1
+                    print("Problem! {:}".format(hmmdata.id[indeces[i][0][j]]))
+                    continue
+                fig3 = plt.figure()
+                tra = np.array(tracks[indeces[i][0][j]+difference].track[np.invert(np.isnan(tracks[indeces[i][0][j]+difference].track['x']))])
+                if len(tra) == 0:
+                    j+=1
+                    print("Track of length 0???")
+                    continue
+                z = tra['frame']-tra[0]['frame']*timestep
+                x = tra['x']*pixel_size
+                y = tra['y']*pixel_size
+                minx = x.min()
+                miny = y.min()
+                maxx = x.max()
+                maxy = y.max()
+                points = np.array([x,y]).T.reshape(-1,1,2)
+                segments = np.concatenate([points[:-1],points[1:]],axis=1)
+                lc = LineCollection(segments, cmap=plt.get_cmap('Spectral'),norm=plt.Normalize(0,z.max()))
+                lc.set_array(z)
+                lc.set_linewidth(2)
+                plt.gca().add_collection(lc)
+                plt.axis([minx-2*pixel_size,maxx+2*pixel_size,miny-2*pixel_size,maxy+2*pixel_size])
+                axcb = fig3.colorbar(lc)
+                axcb.set_label('time in $s$')
+                plt.xlabel(r'x in $\mu m$')
+                plt.ylabel(r'y in $\mu m$')
+                plt.savefig("temp{:}.png".format(j))
+                plt.close()
+                print("Done {:}-{:}".format(i,j))
+                j += 1
+
+
