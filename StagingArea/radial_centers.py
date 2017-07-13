b46514b7dc3fa388c5a1b02ddcef67e778642800
Markus Rose <markus.m.rose@gmail.com>
2017-07-12 23:31:59 -0400

    Added gaussian least squares fit to compare.

--- StagingArea/radial_centers.py
+++ StagingArea/radial_centers.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
 import scipy.ndimage.filters
+from scipy import optimize
 from PIL import Image
 import sys
 
@@ -15,6 +16,56 @@ def gauss2d(coord,mean,std,intensity):
 
 def addNoise(coord,sigtonoise):
     return np.random.normal(0,np.sqrt(1.*np.max(coord)/sigtonoise),size=(len(coord),len(coord[0])))
+
+
+def fitgaussian2d(data, background_mean, user_moments = None):
+    """Returns (height, amplitude, x, y, width_x, width_y) as a numpy array
+    found by least squares fitting of gaussian variables:"""
+    
+    if user_moments == None:
+        initial_params = image_moments(data, background_mean)
+    else:
+        initial_params = user_moments
+    
+    #ravel is a special case for "unraveling" higher dimensional arrays into 1D arrays
+    errorfunction = lambda p: np.ravel((gaussian2d(*p)(*np.indices(data.shape)) - data))
+    p, cov, infodict, errmsg, success = optimize.leastsq(errorfunction, initial_params, full_output=1)
+    return p
+
+
+
+def image_moments(data, mean_background):
+    total = data.sum()
+    Y,X = np.indices(data.shape)
+  
+    x = (X*data).sum()/total
+    y = (Y*data).sum()/total
+    
+    col = data[:, int(x)]
+    width_y = np.sqrt(np.abs((np.arange(col.size)-y)**2*col).sum()/col.sum())
+    
+    row = data[int(y), :]
+    width_x = np.sqrt(np.abs((np.arange(row.size)-x)**2*row).sum()/row.sum())
+    
+    height = mean_background
+    amplitude = data.max() - height
+    
+    return (height, amplitude, x, y, width_x, width_y)
+
+
+
+def gaussian2d(height, amplitude, center_x, 
+               center_y, width_x, width_y):
+    """Return 2d gaussian lamda(x,y) function"""
+    
+    height = float(height)
+    amplitude = float(amplitude)
+    width_x = float(width_x)
+    width_y = float(width_y)
+    
+    return (lambda x,y: height + 
+            amplitude*np.exp(-(((center_x-x)/width_x)**2+((center_y-y)/width_y)**2)/2))
+
 
 def radial_center(I):
 
@@ -97,9 +148,13 @@ if __name__=="__main__":
 
     xc,yc,divI,oDI = radial_center(I)
 
+    param = fitgaussian2d(I, Background)
+
     print xc,yc
+    print param[2],param[3]
     print mean
     print np.array([xc,yc]) - np.array(mean)
+    print np.array([param[2],param[3]]) - np.array(mean)
 
     sys.stdout.flush()
 
@@ -122,5 +177,5 @@ if __name__=="__main__":
     #g = plt.figure(4)
     #plt.plot(I[int(mean[0])])
 
-    plt.show()
+    #plt.show()
 
