0ec6ae891979edc8feaa33af1f296981df955a50
Markus Rose <markus.m.rose@gmail.com>
2017-07-12 23:16:25 -0400

    Corrected SNR for Radial Center trial file

--- StagingArea/radial_centers.py
+++ StagingArea/radial_centers.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 from matplotlib import cm
 import numpy as np
+import scipy.ndimage.filters
 from PIL import Image
 import sys
 
@@ -13,12 +14,13 @@ def gauss2d(coord,mean,std,intensity):
     return np.exp(-((grid[0]-1.*mean[0])**2+(grid[1]-1.*mean[1])**2)/(2.*std**2))*intensity
 
 def addNoise(coord,sigtonoise):
-    return np.random.normal(0,np.sqrt(1.*np.max(coord)**2/sigtonoise),size=(len(coord),len(coord[0])))
+    return np.random.normal(0,np.sqrt(1.*np.max(coord)/sigtonoise),size=(len(coord),len(coord[0])))
 
 def radial_center(I):
 
     m = np.zeros(((len(I)-1)*(len(I[0])-1)))
     divI = np.zeros(((len(I)-1)*(len(I[0])-1)))
+    d_kc = np.zeros(((len(I)-1)*(len(I[0])-1)))
     w = np.zeros(((len(I)-1)*(len(I[0])-1)))
 
     W = 0
@@ -37,40 +39,55 @@ def radial_center(I):
             u = I[i+1,j+1]-I[i,j]
             v = I[i,j+1]-I[i+1,j]
             #print u-v
-            #print i,j,k
+            print i,j,k
             divI[k] = u**2 + v**2
             if u-v == 0:
                 m[k] = 1e10
             else:
                 m[k] = (u+v)/(u-v)
-            d_kc = np.sqrt((xmax-i)**2+(ymax-j)**2)
-            if d_kc == 0:
-                d_kc = 1e-2
-            w[k] = divI[k]/d_kc
-            W += w[k]/(m[k]**2 + 1)
-            M += m[k]*w[k]/(m[k]**2 + 1)
-            M2 += m[k]**2 * w[k]/(m[k]**2 + 1)
-            Wxy += w[k]/(m[k]**2 + 1) * (m[k]*(i+0.5) - (j+0.5))
-            Mxy += m[k]*w[k]/(m[k]**2 + 1) * (m[k]*(i+0.5) - (j+0.5))
+            d_kc[k] = np.sqrt((xmax-i)**2+(ymax-j)**2)
+            if d_kc[k] == 0:
+                d_kc[k] = 1e-2
+
+    origar = np.array(divI,copy=True)
+    divI = np.reshape(divI,(len(I)-1,len(I[0])-1))
+    divI = scipy.ndimage.filters.convolve(divI,np.full((3,3),1))
+    divI = divI.flatten()
+    '''
+    m = np.reshape(divI,(len(I)-1,len(I[0])-1))
+    m = scipy.ndimage.filters.convolve(m,np.full((3,3),1))
+    m = m.flatten()
+    '''
+
+    for k in xrange((len(I)-1)*(len(I[0])-1)):
+        i = int(1.*k/(len(I[0])-1))
+        j = k % (len(I[0])-1)
+        print i,j
+        w[k] = divI[k]/d_kc[k]
+        W += w[k]/(m[k]**2 + 1)
+        M += m[k]*w[k]/(m[k]**2 + 1)
+        M2 += m[k]**2 * w[k]/(m[k]**2 + 1)
+        Wxy += w[k]/(m[k]**2 + 1) * (m[k]*(i+0.5) - (j+0.5))
+        Mxy += m[k]*w[k]/(m[k]**2 + 1) * (m[k]*(i+0.5) - (j+0.5))
 
     delt = W * M2 - M**2
     xc = (Mxy * W - Wxy * M)/delt
     yc = (Mxy * M - Wxy * M2)/delt
 
-    return xc, yc, np.reshape(divI,(len(I)-1,len(I[0])-1))
+    return xc, yc, np.reshape(divI,(len(I)-1,len(I[0])-1)),np.reshape(origar,(len(I)-1,len(I[0])-1))
 
 
 
 if __name__=="__main__":
 
-    size = 20
+    size = 10
 
     mean = np.array([size/2.,size/2.])
-    mean += np.random.random((2))*4. - 2.
-    std = 2
+    mean += np.random.random((2))*size/5. - size/10.
+    std = 3
     intens = 100
-    SNR = 30
-    Background = 10000
+    SNR = 100
+    Background = 100
 
     I = np.zeros((size,size),dtype=np.float_)
     I = gauss2d(I,mean,std,intens)
@@ -78,7 +95,7 @@ if __name__=="__main__":
     I += Background
     I[I<0] = 0
 
-    xc,yc,divI = radial_center(I)
+    xc,yc,divI,oDI = radial_center(I)
 
     print xc,yc
     print mean
@@ -88,14 +105,22 @@ if __name__=="__main__":
 
     imI = Image.fromarray(np.uint8(cm.gist_earth((I-np.min(I))/(np.max(I)-np.min(I)))*255))
     imDivI = Image.fromarray(np.uint8(cm.gist_earth((divI-np.min(divI))/(np.max(divI)-np.min(divI)))*255))
+    imODI =  Image.fromarray(np.uint8(cm.gist_earth((oDI-np.min(oDI))/(np.max(oDI)-np.min(oDI)))*255))
+
 
     f = plt.figure(1)
     plt.imshow(imI)
+    
+    '''
+    f2 = plt.figure(2)
+    plt.imshow(imODI)
+    '''
 
     f1 = plt.figure(3)
     plt.imshow(imDivI)
 
-    g = plt.figure(2)
-    plt.plot(I[int(mean[0])])
+    #g = plt.figure(4)
+    #plt.plot(I[int(mean[0])])
 
     plt.show()
+
